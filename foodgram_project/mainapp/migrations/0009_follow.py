5c9533231bf3c8720a6958a4cc08456a4c8502b1
Vilbert55 <a5735402@gmail.com>
2020-11-10 21:55:36 +0300

    1.0

--- foodgram_project/mainapp/migrations/0009_follow.py
+++ foodgram_project/mainapp/migrations/0009_follow.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('mainapp', '0008_favorite'),
+        ('mainapp','0008_favorite'),
     ]
 
     operations = [
